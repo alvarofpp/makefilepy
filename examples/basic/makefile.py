4483c4213f9dad44b008d5b01f137f894377dfb9
Álvaro Paiva <alvaro.paiva@meliuz.com.br>
2022-11-13 19:30:12 -0300

    chore: Adds examples

--- examples/basic/makefile.py
+++ examples/basic/makefile.py
@@ -3,22 +3,20 @@ from makefilepy import Command, Makefile, Variable
 
 variable = Variable(
     key='test',
-    value='test'
+    value='test',
 )
-
 command = Command(
     target='up',
     prerequisites=[
-        'docker-compose.yml'
+        'docker-compose.yml',
     ],
     recipes=[
-        'docker-compose up'
+        'docker-compose up',
     ],
     phony=True,
 )
-
 makefile = Makefile()
 
 makefile.variables.append(variable)
 makefile.commands.append(command)
-makefile.build(filename='test.txt')
+makefile.build(filename='Makefile')
